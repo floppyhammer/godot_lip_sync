4d1221b110f866ecc93af5d4318cf6f5f975bc37
floppyhammer <tannhauser_chen@outlook.com>
2023-12-08 10:16:13 +0800

    Improve FFT performance

--- addons/lip_sync/algorithm.gd
+++ addons/lip_sync/algorithm.gd
@@ -52,21 +52,27 @@ static func hamming(sample_array: Array[float]):
 	return;
 
 static func rfft(sample_array: Array[float], reverse: bool = false, positive: bool = true):
-	var n: int = sample_array.size()
-	var cmp_array = []
-	for i in range(n):
-		cmp_array.append(Vector2(sample_array[i], 0.0))
+	var N: int = sample_array.size()
+	
+	var cmp_array: Array[Vector2] = []
+	cmp_array.resize(N)
+	
+	for i in range(N):
+		cmp_array[i] = Vector2(sample_array[i], 0.0)
+	
 	fft(cmp_array, reverse)
+	
 	if positive:
-		for i in range(n):
+		for i in range(N):
 			sample_array[i] = abs(cmp_array[i].x)
 	else:
-		for i in range(n):
+		for i in range(N):
 			sample_array[i] = cmp_array[i].x
 	if reverse:
-		var inv_n: float = 1.0 / float(n)
-		for i in range(n):
+		var inv_n: float = 1.0 / float(N)
+		for i in range(N):
 			sample_array[i] *= inv_n
+			
 	return
 
 # Reference (https://caddi.tech/archives/836) 
@@ -74,18 +80,32 @@ static func fft(a: Array, reverse: bool):
 	var N: int = a.size()
 	if N == 1:
 		return
-	var b: Array = []
-	var c: Array = []
+	
+	var b: Array[Vector2] = []
+	b.resize(int(ceil(N * 0.5)))
+	
+	var c: Array[Vector2] = []
+	c.resize(int(N * 0.5))
+	
+	var b_idx: int = 0
+	var c_idx: int = 0
+	
 	for i in range(N):
 		if i % 2 == 0:
-			b.append(a[i])
+			b[b_idx] = a[i]
+			b_idx += 1
 		elif i % 2 == 1:
-			c.append(a[i])
-	fft(b, reverse);
-	fft(c, reverse);
+			c[c_idx] = a[i]
+			c_idx += 1
+
+	fft(b, reverse)
+	fft(c, reverse)
+	
 	var circle: float = -PI2 if reverse else PI2
 	for i in range(N):
-		a[i] = b[i % (N / 2)] + ComplexCalc.cmlt(c[i % (N / 2)], ComplexCalc.cexp(Vector2(0, circle * float(i) / float(N))));
+		var mod = i % (N / 2)
+		a[i] = b[mod] + ComplexCalc.cmlt(c[mod], ComplexCalc.cexp(Vector2(0, circle * float(i) / float(N))));
+	
 	return
 
 static func lifter(sample_array: Array[float], level: int):
